a90816f3f7b4466f2ba98ec5829833c11b5d9cc2
mclayt30 <mclayton7586@gmail.com>
2023-04-14 18:07:06 -0400

    Added Models

--- Machine Learning Project.py
+++ Machine Learning Project.py
@@ -64,15 +64,27 @@ model = tf.keras.models.Sequential()
 # model.add(layers.Dense(10))
 
 # Model 3 with Dropout: 99.21% accuracy, loss = 0.0259
+# model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Dropout(0.2))
+# model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+# model.add(layers.MaxPooling2D((2, 2)))
+# model.add(layers.Dropout(0.2))
+# model.add(layers.Flatten())
+# model.add(layers.Dense(128, activation='relu'))
+# model.add(layers.Dropout(0.2))
+# model.add(layers.Dense(10))
+
+# Model 3 with Batch Normalization: 98.95% accuracy, loss = 0.0361
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Dropout(0.2))
+model.add(layers.BatchNormalization())
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Dropout(0.2))
+model.add(layers.BatchNormalization())
 model.add(layers.Flatten())
 model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dropout(0.2))
+model.add(layers.BatchNormalization())
 model.add(layers.Dense(10))
 
 
@@ -83,6 +95,8 @@ model.compile(optimizer = 'adam',
 
 epochs = 5
 history = model.fit(x_train, train_labels, epochs = epochs, validation_data = (x_val, y_val))
+
+ 
 
 model.summary()
 
@@ -95,24 +109,33 @@ val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 
+model.save('model3B')
+
+# epochs_range = range(epochs)
+
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+
+# plt.legend(loc='lower right')
+# plt.title('Training and Validation Accuracy')
+
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
 
 
-epochs_range = range(epochs)
-
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Validation Loss')
+# plt.show()
 
 
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
 
+
+
+
+
+# model1 = tf.keras.models.load_model('model1')
+# model1.summary()
+# model1.evaluate(x_test, test_labels, verbose = 2)
